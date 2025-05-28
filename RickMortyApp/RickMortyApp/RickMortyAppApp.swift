d734f4a2ed35ee30342b3a4754569e24d0c70f4f
Guido Fabio <gui2_fabio@hotmail.com>
2025-05-28 13:10:53 -0300

    add: initial configuration

---
+++ RickMortyApp/RickMortyApp/RickMortyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickMortyAppApp.swift
+//  RickMortyApp
+//
+//  Created by Guido Fabio on 28/5/25.
+//
+
+import SwiftUI
+
+@main
+struct RickMortyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
